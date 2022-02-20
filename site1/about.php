c16fb16360a0c2217d4f80692404bc772fcfe6b2
VicoLDaniel <99674571+VicoLDaniel@users.noreply.github.com>
2022-02-20 13:00:36 +0200

    Add files via upload

---
+++ site1/about.php
@@ -0,0 +1,34 @@
+      <?php
+include"log.php";
+
+include"reg.php";
+
+        ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="point.css">
+      <meta charset="utf-8">
+      <title>📚𝗔𝗯𝗼𝘂𝘁</title>
+
+      <?php
+include"jq.php";
+        ?>
+   </head>
+   <body>
+      <body >
+         <a href="#" id="registration" >Registration</a>
+         <a href="#" id="login" >Login</a>
+
+         <div id="box1"  >
+            <a id="home" href="refresh.php">🏠𝗛𝗼𝗺𝗲</a>
+            <a id="news" href="new.php">📰𝗡𝗲𝘄𝘀</a>
+            <a id="contact" href="contact.php">📞𝗖𝗼𝗻𝘁𝗮𝗰𝘁</a>
+            <a id="about1" href="#">📚𝗔𝗯𝗼𝘂𝘁</a>
+         </div>
+         </br></br>
+         <div id="tabel"></br><a id="text1">the name of this project is Mark3</a></div>
+   </body>
+</html>
+<b><h1><a href='SNAKE.exe' style="color:white">OMG</a></h1></b>
---
+++ site1/contact.php
@@ -0,0 +1,34 @@
+ <?php
+include"log.php";
+
+include"reg.php";
+
+        ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="point.css">
+      <meta charset="utf-8">
+      <title>📞𝗖𝗼𝗻𝘁𝗮𝗰𝘁</title>
+
+<?php
+include"jq.php";
+        ?>
+   </head>
+
+      <body >
+         <a href="#" id="registration" >Registration</a>
+         <a href="#" id="login" >Login</a>
+
+         <div id="box1"  >
+            <a id="home" href="refresh.php">🏠𝗛𝗼𝗺𝗲</a>
+            <a id="news" href="new.php">📰𝗡𝗲𝘄𝘀</a>
+            <a id="contact1" href="#">📞𝗖𝗼𝗻𝘁𝗮𝗰𝘁</a>
+            <a id="about" href="about.php">📚𝗔𝗯𝗼𝘂𝘁</a>
+         </div>
+         </br></br>
+         <div id="tabel"></br><a id="text1"> sorry but we dont have contacts</a> </div>
+   </body>
+
+</html>
